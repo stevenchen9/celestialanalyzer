a17ed28bf78f338ec6ba029ec47013fd99718a63
jordandkaufman <37273384+jordandkaufman@users.noreply.github.com>
2018-03-12 00:23:57 -0400

    Made it so that you  an completely isolate an image of a galaxy or celestial body based on thresholds and area. Im gonna work on finding the larges object and then isolating it.

--- Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
+++ Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
@@ -3,9 +3,12 @@
 
 import matlab.io.*
 imMat=imread('frame-irg-003712-3-0187.jpg');
-BW=im2bw(imMat,.4);
+imMat=imgaussfilt(imMat,4);
+BW=im2bw(imMat,.3);
+BW=bwareaopen(BW,5000);
+figure, imshow(imMat);
 subplot(1,2,1), imshow(imMat);
 subplot(1,2,2), imshow(BW);
 
 % this is a test
-%hi mango
+% 
