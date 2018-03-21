0c9ed2c969c353fa6ebbfa97b0b0bff1bf93b7ea
jordandkaufman <37273384+jordandkaufman@users.noreply.github.com>
2018-03-21 18:41:53 -0400

    Takes image, applies filter, turns into binary image

--- Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
+++ Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
@@ -3,12 +3,19 @@
 
 import matlab.io.*
 imMat=imread('frame-irg-003712-3-0187.jpg');
-imMat=imgaussfilt(imMat,4);
+figure, imshow(imMat)
+imMat=imgaussfilt(imMat,3);
+figure, imshow(imMat)
 BW=im2bw(imMat,.3);
-BW=bwareaopen(BW,5000);
-figure, imshow(imMat);
-subplot(1,2,1), imshow(imMat);
-subplot(1,2,2), imshow(BW);
+figure, imshow(BW);
+numObj=bwconncomp(BW);
+%val = max(numObj.PixelIdxList)
+
+BW=bwareaopen(BW,5000);x
+figure, imshow(BW);
+%figure, imshow(imMat);
+%subplot(1,2,1), imshow(imMat);
+%subplot(1,2,2), imshow(BW);
 
 % this is a test
 % 
