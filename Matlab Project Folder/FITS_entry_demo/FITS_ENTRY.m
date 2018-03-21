38e60b67eb4c6aac0760f4bcd734156ca8d4427d
jordandkaufman <37273384+jordandkaufman@users.noreply.github.com>
2018-03-21 19:07:33 -0400

    used graythresh feature to obtain threshold value automatically

--- Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
+++ Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
@@ -6,13 +6,14 @@ imMat=imread('frame-irg-003712-3-0187.jpg');
 figure, imshow(imMat)
 imMat=imgaussfilt(imMat,3);
 figure, imshow(imMat)
-BW=im2bw(imMat,.3);
+level=graythresh(imMat);
+BW=im2bw(imMat,level);
 figure, imshow(BW);
-numObj=bwconncomp(BW);
+%numObj=bwconncomp(BW);
 %val = max(numObj.PixelIdxList)
 
-BW=bwareaopen(BW,5000);x
-figure, imshow(BW);
+%BW=bwareaopen(BW,5000);x
+%figure, imshow(BW);
 %figure, imshow(imMat);
 %subplot(1,2,1), imshow(imMat);
 %subplot(1,2,2), imshow(BW);
