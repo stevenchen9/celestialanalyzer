e2a4fe247915413d7493dacb2eb9964b618a3405
jordandkaufman <37273384+jordandkaufman@users.noreply.github.com>
2018-04-13 17:28:16 -0400

    Commented some things away and worked on intensity profile, struggling mightily

--- Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
+++ Matlab Project Folder/FITS_entry_demo/FITS_ENTRY.m
@@ -10,23 +10,24 @@ distance = input('Enter image distance: ');
 
 imMat=imread(filename);
 
-size = getsize(imMat, maginification, distance);
+%size = getsize(imMat, maginification, distance);
 % shows the original image
-figure, imshow(imMat)
+%figure, imshow(imMat)
 % makes background stars dissapears for galaxies
 imMat=imgaussfilt(imMat,3);
 figure, imshow(imMat)
 % turns it into a black and white photo
 level=graythresh(imMat);
 grayscale=rgb2gray(imMat);
+
 %takes rgb and turn to grayscale
 
-intensity profile
-profile = makintprof(grayscale);
-figure, mesh(profile);
+%intensity profile
+%profile = makintprof(grayscale);
+%figure, mesh(profile);
 
 BW=im2bw(imMat,level);
-figure, imshow(BW);
+%figure, imshow(BW);
 %s = regionprops(BW,'centroid');
 %plot(s)
 numObj=bwconncomp(BW);
@@ -44,12 +45,22 @@ stats = regionprops('table',BW,'Centroid','MajorAxisLength','MinorAxisLength','E
 xcoord= round(stats{1,1}(1,2));
 ycoord= round(stats{1,1}(1,1));
 BW((xcoord-5):(xcoord+5),(ycoord-5):(ycoord+5))=0;
+figure, imshow(grayscale);
 figure, imshow(BW);
+%first attempt polar plot from COM of galaxy
+
+theta=0:0.01:2*pi;
+x1=[xcoord,cos(theta)*round(stats{1,2})];
+y1=[xcoord,sin(theta)*round(stats{1,2})];
+rho=mean(improfile(grayscale,x1,y1));
+figure, plot(rho);
+
 
 %intensity plot from image center
 %using improfile and a line segment the length of the major axis we can see
 %the intensity of the entire 
-xi=[xcoord, xcoord];
-yi=[ycoord, ycoord+4*round(stats{1,2})];
-improfile(imMat,xi,yi);
+xi=[xcoord,xcoord];
+yi=[ycoord,ycoord-round(stats{1,2})];
+c=improfile(grayscale,xi,yi);
+figure, plot(c);
 
