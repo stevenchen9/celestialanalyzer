a39513edd3f4aad358c89d2e11296d59bdd2281f
jordandkaufman <37273384+jordandkaufman@users.noreply.github.com>
2018-04-17 00:29:00 -0400

    moved images

--- Matlab Project Folder/FITS_entry_demo/makeintprof.m
+++ Matlab Project Folder/FITS_entry_demo/makeintprof.m
@@ -1,17 +1,19 @@
-function intprofile = makeintprof(grayscale)
-
+%function intprofile = makeintprof(grayscale)
+imMat=imread('test1.jpg');
+grayscale=rgb2gray(imMat);
 %intensity profile
 figure, imshow(grayscale);
 %improfile(grayscale,(1:1488),(1:1488))
 %have created a 3d intensity plot which will aid analysis
-inrprofile = zeros(1488,2048);
+intprofile = zeros(1488,2048);
 x=[1,2047];
 y=[1,1];
-figure, plot(improfile(grayscale,x,y))
+figure, plot(improfile(grayscale,x,y));
 for j=1:2040
     for i=1:1480
         intprofile(i,j)=grayscale(i,j);
     end
 end
-end
+figure, mesh(intprofile);
+%end
 
